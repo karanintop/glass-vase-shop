ffc22572c9361117671325c1ef27600edf87a19a
karanintop <adsdsasdasdaewrewrrwerwerwe@gmail.com>
2025-07-18 11:33:25 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const staticCacheName = 'CacheSpace';
+
+const assetUrls = [
+  '/icons',
+  '/images',
+  '/index1.html', 
+  '/style.css',
+  '/Order.html',
+  '/orderstyle.css',
+  '/Goods.html',
+  '/goodsstyle.css',
+]
+
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(staticCacheName)
+  await cache.addAll(assetUrls)
+  console.log('загрузилось')
+  
+})
+
+self.addEventListener('activate', event => {
+  console.log('[SW]: activate')
+})
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch', event.request.url)
+  event.respondWith(cacheFirst(event.request))
+})
+
+async function cacheFirst(request) {
+  const cached = await caches.match(request)
+  return cached ?? await fetch(request)
+}
